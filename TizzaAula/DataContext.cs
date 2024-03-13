acfcaa91b92600dead7f2bc02d379109906c3dc4
eduardo <eduardocmeneghel@gmail.com>
2024-03-13 20:47:43 -0300

    Primeiro Commit

---
+++ TizzaAula/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TizzaAula
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+              
+        }
+
+        public DbSet<Pizzaria> Pizzaria { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pizzaria>().HasKey(p => p.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ TizzaAula/Pizzarias/ServPizzaria.cs
@@ -0,0 +1,36 @@
+﻿namespace TizzaAula
+{
+    public interface IServPizzaria
+    {
+        void Inserir(Pizzaria pizzaria);
+        void Alterar(int id, AlterarPizzariaDTO alterarPizzariaDto);
+    }
+
+    public class ServPizzaria : IServPizzaria
+    {
+        private DataContext _dataContext;
+
+        public ServPizzaria(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public void Inserir(Pizzaria pizzaria)
+        {
+            _dataContext.Add(pizzaria);
+
+            _dataContext.SaveChanges();
+        }
+
+        public void Alterar(int id, AlterarPizzariaDTO alterarPizzariaDto)
+        {
+            var pizzaria = _dataContext.Pizzaria.Where(p => p.Id == id).FirstOrDefault();
+
+            pizzaria.Nome = alterarPizzariaDto.Nome;
+            pizzaria.Endereco = alterarPizzariaDto.Endereco;
+            pizzaria.Telefone = alterarPizzariaDto.Telefone;
+
+            _dataContext.SaveChanges();
+        }
+    }
+}
