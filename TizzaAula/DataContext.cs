73f8f2320c6c9bdacda119d1c226b6c1ac1a654d
eduardo <eduardocmeneghel@gmail.com>
2024-03-20 19:20:54 -0300

    Mapeamento do processo de promover

--- TizzaAula/DataContext.cs
+++ TizzaAula/DataContext.cs
@@ -10,10 +10,15 @@ namespace TizzaAula
         }
 
         public DbSet<Pizzaria> Pizzaria { get; set; }
+        public DbSet<Promover> Promover { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Pizzaria>().HasKey(p => p.Id);
+            
+            modelBuilder.Entity<Promover>().HasKey(p => p.Id);
+            modelBuilder.Entity<Promover>().HasOne(p => p.Pizzaria).WithMany()
+                .HasForeignKey(p => p.CodigoPizzaria);
 
             base.OnModelCreating(modelBuilder);
         }
